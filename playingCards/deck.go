7cd3c44dcba2a774d1af6b11200f885c97c60bb3
David Rajcher <david.rajcher@gmail.com>
2024-07-21 12:16:11 +0200

    deal multiple hands

--- playingCards/deck.go
+++ playingCards/deck.go
@@ -9,38 +9,51 @@ import (
 
 type deck []string
 
-func (deck deck) print() {
+func (deck deck) print(name string) {
+	if len(name) > 0 {
+		fmt.Println(name)
+	}
 	for i, card := range deck {
 		fmt.Println(i, card)
 	}
 }
-
-func (deck *deck) appendCard (card string)  {
-	*deck = append(*deck, card)
-}
-
-func createShuffledDeck() []string {
+func createShuffledDeck() deck {
 	suits := []string{"♠", "♣", "♥", "♦"}
 	ranks := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 
-	var deck []string
+	var d deck
 	
 	// Populate the deck
 	for _, suit := range suits {
 		for _, rank := range ranks {
 			card := rank + suit
-			deck = append(deck, card)
+			d = append(d, card)
 		}
 	}
+
+	d = d.shuffleDeck()
 	
-	//Seed the random number generator
+	return d
+}
+
+func (d deck) shuffleDeck() deck {
+	// Seed the random number generator
 	rand.Seed(time.Now().UnixNano())
 
 	// Shuffle the deck
-	rand.Shuffle(len(deck), func(i, j int) {
-		deck[i], deck[j] = deck[j], deck[i]
+	rand.Shuffle(len(d), func(i, j int) {
+		d[i], d[j] = d[j], d[i]
 	})
-	
-	return deck
+	return d
 }
 
+func (d deck) dealHand(handSize int, numPlayers int) (deck, []deck) {
+	var players = make([]deck, numPlayers)
+	
+	for i := 0; i < numPlayers; i++ {
+		players[i] = d[:handSize]
+		d = d[handSize:]
+	}
+	
+	return d, players
+}
--- playingCards/main.go
+++ playingCards/main.go
@@ -2,7 +2,19 @@ package main
 
 
 func main() {
-	var cards deck = createShuffledDeck()
+	
+	var cards deck
 
-	cards.print()
+	cards = createShuffledDeck()
+
+	handSize := 5
+	numPlayers := 4
+	
+	remainingDeck, playersHands := cards.dealHand(handSize, numPlayers)
+	
+	remainingDeck.print("cards")
+
+	for _, hand := range playersHands {
+		hand.print("hand")
+	}
 }
